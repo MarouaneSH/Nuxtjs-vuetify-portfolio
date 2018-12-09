35b03e87b1dda7a05be6da0f4a4a23afc6266533
Marouane Souah <marouanesouah@gmail.com>
2018-12-09 22:15:10 +0100

    Scrolling animation

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,19 @@
   <div class="home_content">
      <div class="home_content_right_pan"></div>
       <v-container>
+          <div class="home_content_scroll">
+            <img src="svg/scroll.svg" alt="">
+           <p> scroll to discover</p>
+          </div>
           <v-layout row>
               <v-flex x6>
                   <app-intro ref="intro" @onAnimationFinish="animateHomePage" />
               </v-flex>
               <v-flex x6>
+                  <div class="home_content_nav">
+                      <span>Works</span>
+                      <span>Skills</span>
+                  </div>
               </v-flex>
           </v-layout>
       </v-container>
@@ -38,6 +46,15 @@ export default {
 
           }
         })
+        .add({
+           targets : ".home_content_scroll",
+           opacity : 1,
+           easing: 'easeOutExpo',
+           translateY : 0,
+           duration: 1500,
+           elasticity: 600,
+            scale: [0, 1],
+        })
     },
   },
   components: {
@@ -50,13 +67,42 @@ export default {
 <style scoped lang="scss">
 .home_content {
   position: relative;
+  height: 100vh;
+  overflow: hidden;
   &_right_pan{
     width: 40%;
     position: absolute;
     right: -50%;
     height: 100vh;
     background: #03214c;
+    z-index: -1;
   }
+}
+
+.home_content {
+    &_scroll{
+        position: absolute;
+        left: 0;
+        right: 0;
+        opacity: 0;
+        bottom: 40px;
+        margin: auto;
+        text-align: center;
+        color: white;
+        transform : translateY(40px);
+        img {
+          width: 30px;
+        }
+    }
+    &_nav {
+      color: white;
+      text-align: right;
+      span{
+        font-size: 20px;
+        font-weight: bold;
+        margin:  0 5px;
+      }
+    }
 }
 </style>
 
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faGithub } from '@fortawesome/fontawesome-free-brands'
+import { faGithub, faFacebook } from '@fortawesome/fontawesome-free-brands'
 import { fas, faEdit } from '@fortawesome/free-solid-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
@@ -9,7 +9,7 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas,faGithub)
+library.add(fas,faGithub,faFacebook)
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
