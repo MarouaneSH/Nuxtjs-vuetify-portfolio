5c3d65f60c6449d80f8843add08f77a916891f77
Marouane Souah <marouanesouah@gmail.com>
2018-12-09 12:42:16 +0100

    Trigger animation from intro to home

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home_content">
-      <app-intro ref="intro" />
+      <v-container>
+          <v-layout row>
+              <v-flex x6>
+                  <app-intro ref="intro" @onAnimationFinish="animateHomePage" />
+              </v-flex>
+              <v-flex x6>
+                 <div class="home_content_right_pan"></div>
+              </v-flex>
+          </v-layout>
+      </v-container>
   </div>
 </template>
 
@@ -9,15 +18,45 @@ import appIntro from '~/components/intro.vue'
 
 export default {
   mounted() {
-      this.$refs.intro.animate_intro();
+    //starting intro animation
+     this.$refs.intro.animate_intro();
   },
-
+  methods : {
+     animateHomePage() {
+      let _this = this;
+      this.$anime
+        .timeline()
+        .add({
+          targets : ".home_content_right_pan",
+          translateX: "-100%",
+          delay : 200,
+          duration : 2500,
+          complete : function() {
+            alert("dssds");
+          }
+        })
+    },
+  },
   components: {
     appIntro,
 
   }
 }
 </script>
+
+<style scoped lang="scss">
+.home_content {
+  position: relative;
+  &_right_pan{
+    width: 50%;
+    position: absolute;
+    right: -50%;
+    height: 100vh;
+    background: #03214c;
+  }
+}
+</style>
+
 
 
 
